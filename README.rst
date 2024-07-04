b07052a61e1539e90b0e96a74b65a911bb50f502
realBombshock <mohammadjavadyousefi3@gmail.com>
2024-07-04 15:48:10 +0330

    fixed the half profile pic problem and added a virtualenv for more simple use and changed the clock to 24 hour format\

--- README.rst
+++ README.rst
@@ -20,26 +20,29 @@ between multiple people. It takes in a text file of the form
    plain
 
 and produces a sequence of discord messages, like those found in Beluga
-or Mr. P Solver videos. Messages from the same person stack: for
-example, in the chat above, the sequence of messages produced by the
+videos. Messages from the same person stack:
+for example, in the chat above, the sequence of messages produced by the
 first chats of Jim are “Hey Bob” then “Hey Bob (newline) What kind of
 chocolate…” and then “Hey Bob (newline) What kind of chocolate…
 (newline) Do you find in airports?”
 
-To run
+To run:
 
-1. ``pip install -r requirements.txt``
+1. use the Virtualenv with the name of VenvDiscordChatGen as Python interpreter so you don't need to download
+the requirements! but if you want to use your own python version use ``pip install -r requirements.txt``
+but remember this code only works in python 3.10 and 3.11 (i tested in 3.10) it will not work with 3.12!
+
 2. Add all desired profile pictures to the ``profile_pictures/`` folder.
-3. Inside the ``profile_pictures/`` folder, update the
-   ``profile_pic_dict.json`` with names (corresponding to the names in
-   whatever script txt file you want to convert) and corresponding
-   profile pictures.
+
+3. Inside the ``profile_pictures/`` folder,then update the ``profile_pic_dict.json``
+with names (corresponding to the names inwhatever script txt file you want to convert)
+and corresponding profile pictures.
+
 4. Inside the repository folder, run ``python generate_chat.py``. It
-   will prompt you to choose a ``.txt`` file of a script (format
-   outlines above) somewhere on your computer. The images will be saved
-   in a newly created ``chat/`` folder with the form ``007T.png`` where
-   the first 3 numbers represent the image number in the entire sequence
-   and the letter is the first letter of speakers name.
+will prompt you to choose a ``.txt`` file of a script like the ``Chat Example File.txts``
+The images will be savedin a newly created ``chat/`` folder with the form ``007T.png`` where
+the first 3 numbers represent the image number in the entire sequence
+and the letter is the first letter of speakers name.
 
 The file naming convention for saved ``.PNG`` chats is used because
 videos (very likely) have less than 1000 messages and the three digits
--- generate_chat.py
+++ generate_chat.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageFont, ImageDraw 
 from pilmoji import Pilmoji
-import sys
-import datetime
+
+from datetime import datetime, timedelta
 import os
 from tkinter import Tk, filedialog
 import json
@@ -41,25 +41,31 @@ with open('profile_pictures/profile_pic_dict.json') as file:
   profile_pic_dict = json.loads(file.read())
 
 def generate_chat(messages, name, time, profpic_file):
+
     name_text = name
-    time_text = f'Today at {time} PM'
+    time_text = f'Today at {time}'
     time_position = (NAME_POSITION[0] + name_font.getlength(name) + NAME_TIME_SPACING, TIME_POSITION_Y)
-    # Open proile picture
+    
+    # Open and resize profile picture
     prof_pic = Image.open(profpic_file)
-    prof_pic.thumbnail([sys.maxsize, PROFPIC_WIDTH], Image.ANTIALIAS)
+    prof_pic = prof_pic.resize((PROFPIC_WIDTH, PROFPIC_WIDTH), Image.ANTIALIAS)
+    
     # Create profile picture mask
-    mask = Image.new("L", prof_pic.size, 0)
+    mask = Image.new("L", (PROFPIC_WIDTH, PROFPIC_WIDTH), 0)
     draw = ImageDraw.Draw(mask)
     draw.ellipse([(0, 0), (PROFPIC_WIDTH, PROFPIC_WIDTH)], fill=255)
+    
     # Create template and draw on template
     template = Image.new(mode='RGBA', size=(WORLD_WIDTH, WORLD_HEIGHTS[len(messages)-1]), color=WORLD_COLOR)
-    template.paste(prof_pic, (36,45), mask)
+    template.paste(prof_pic, PROFPIC_POSITION, mask)
     template_editable = ImageDraw.Draw(template)
     template_editable.text(NAME_POSITION, name_text, NAME_FONT_COLOR, font=name_font)
     template_editable.text(time_position, time_text, TIME_FONT_COLOR, font=time_font)
+    
     for i, message in enumerate(messages):
         with Pilmoji(template) as pilmoji:
             pilmoji.text(MESSAGE_POSITIONS[i], message.strip(), MESSAGE_FONT_COLOR, font=message_font)
+    
     return template
 
 def get_filename():
@@ -69,41 +75,50 @@ def get_filename():
     return filedialog.askopenfilename()
 
 def save_images(lines, init_time, dt=30):
-    os.system(f'mkdir chat')
+    os.makedirs('chat', exist_ok=True)
     name_up_next = True # first line starts with name
     current_time = init_time
     current_name = None
     current_lines = []
     msg_number = 1
+    
     for line in lines:
         # If line is empty (signifying end of message chain)
-        if line=='':
-            name_up_next=True
+        if line == '':
+            name_up_next = True
             current_lines = []
             continue
+        
         # lines that are commented out
-        if line[0]=='#':
+        if line.startswith('#'):
             continue
+        
         # If currently on a name line
         if name_up_next:
             current_name = line.split(':')[0]
             name_up_next = False
             continue
+        
         # otherwise append message
         current_lines.append(line)
-        image = generate_chat(messages = current_lines,
-                              name = current_name,
-                              time = f'{current_time.hour % 12}:{current_time.minute}',
-                              profpic_file = f'profile_pictures/{profile_pic_dict[current_name]}')
+        
+        image = generate_chat(
+            messages=current_lines,
+            name=current_name,
+            time=current_time.strftime("%H:%M"),
+            profpic_file=f'profile_pictures/{profile_pic_dict[current_name]}'
+        )
+        
         image.save(f'chat\\{msg_number:03d}{current_name[0]}.png')
+        
         # update time and msg_number for saving
-        current_time+=datetime.timedelta(0,dt)
-        msg_number+=1
+        current_time += timedelta(seconds=dt)
+        msg_number += 1
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     filename = get_filename()
     with open(filename, encoding="utf8") as f:
         lines = f.read().splitlines()
-    current_time = datetime.datetime.now()
+    
+    current_time = datetime.now()
     save_images(lines, init_time=current_time)
